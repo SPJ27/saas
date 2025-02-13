d31fcb2508c52f0c9c7b451fc9efff6e874c01e6
Saksham Jain <saksham.khatod27@gmail.com>
2025-02-13 16:38:12 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/js/script.js',
+        destination: 'https://datafa.st/js/script.js',
+      },
+      {
+        source: '/api/events',
+        destination: 'https://datafa.st/api/events',
+      },
+    ]
+  },
 export default nextConfig;
